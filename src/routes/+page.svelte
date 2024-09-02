dd9ce5d712545d802dde5de1e5ee6f4f410b3022
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-09-02 18:43:12 -0400

    feat: add wallet icon to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import IconWallet from '~icons/mdi/Wallet';
+</script>
+
 <svelte:head>
 	<title>e-Wallet</title>
 </svelte:head>
@@ -5,7 +9,10 @@
 <div class="hero" style="min-height: calc(100vh - 96px);">
 	<div class="hero-content text-center">
 		<div class="max-w-lg">
-			<h1 class="text-5xl font-bold">e-Wallet</h1>
+			<h1 class="flex items-center justify-center gap-2 text-5xl font-bold">
+				<IconWallet />
+				e-Wallet
+			</h1>
 			<h2 class="mt-2 text-4xl font-bold">Sua carteira digital</h2>
 			<p class="py-6">
 				Gerencie suas transações de cartão de crédito, e fique por dentro das suas finanças.
