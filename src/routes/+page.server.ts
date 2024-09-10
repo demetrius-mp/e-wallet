bfb3bd71add89696ea7fe8d5531213246c170831
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-09-09 20:15:41 -0400

    feat: redirect to app if user is authenticated

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load = (async (e) => {
+	const { currentUser } = await e.parent();
+
+	if (currentUser) {
+		redirect(302, '/app');
+	}
+
+	return {};
+}) satisfies PageServerLoad;
